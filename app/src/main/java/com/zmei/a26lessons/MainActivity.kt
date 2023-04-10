1644536ba99cfeccbeac5fc6001f838087e56dc3
arhizmei <arhizmei@gmail.com>
2023-04-10 16:12:59 +0300

    work1

--- app/src/main/java/com/zmei/a26lessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a26lessons/MainActivity.kt
@@ -2,10 +2,24 @@ package com.zmei.a26lessons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zmei.a26lessons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bind : ActivityMainBinding
+    private val adapter = PlantAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        init()
+    }
+
+    private fun init(){
+        bind.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+        }
     }
 }
